c810fc739bee4969af5f8ce0b04986fb1a374a9c
Kevin <Kevin.Lu56@myhunter.cuny.edu>
2019-11-05 10:12:06 -0800

    Added user class

--- Pokemon/pokemonGame.py
+++ Pokemon/pokemonGame.py
@@ -7,11 +7,12 @@
 #               - Separating project so it is well organized and structured.
 # ----------------------------------------------------------------------------------------
 
-from flask import render_template, request
+from flask import render_template, request, flash
 from Pokemon import app
 
 import random
-from Pokemon import pokemonName
+from Pokemon import pokemonName     # Importing the dictionary name so we can check user answer.
+from Pokemon import user            # Importing User class.
 
 generateNumberForImage = random.randrange(492)
 
@@ -23,5 +24,15 @@ def renderPokemonImage():
 # Checking answers taken from users.
 @app.route('/pokemon', methods=("GET", "POST"))
 def checkPokemonName(name):
-    userAnswer = request.form["pokeName"]
-    pass
+    
+    newUser = user.User()
+
+    if (request.method == "POST"):
+        userAnswer = request.form["pokeName"]
+        
+        if (pokemonName.pokemonNames[userAnswer] == generateNumberForImage):
+            newUser.incrementScore()
+            flash(userAnswer)
+            return (render_template("pokemon.html", title="Pokemon", imageNumber=str(generateNumberForImage) + ".png"))
+    else:
+        flash("Please Enter Pokemon Name")
---
+++ Pokemon/user.py
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------------------------------
+# Author: Kevin Lu
+# Date: 11/5/2019
+# File: user.py
+# Purpose: File for user functions such as handling scores and guessing answers.
+# Modification: - Added basic member function of a user class.
+# ----------------------------------------------------------------------------------------
+
+class User:
+
+    def __init__(self):
+        self.userScore = 0
+    
+    def getScore (self):
+        return self.score
+
+    def setAnswer (self, answer):
+        self.userAnswer = answer
+    
+    def getAnswer (self):
+        return self.userAnswer
+    
+    def incrementScore (self):
+        return self.userScore += 1
