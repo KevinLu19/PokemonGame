8508981dc57874f8df3f57c205909dd2a46fd3b0
Kevin <kevinxz01@gmail.com>
2021-11-13 12:54:50 -0600

    Add pokemon names onto sqlite3.

--- Pokemon/__init__.py
+++ Pokemon/__init__.py
@@ -1,17 +1,26 @@
 from flask import Flask, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-import os
+import sqlalchemy
 import secret_key
 
 # Creating Flask instance
 app = Flask(__name__)
 app.config["SECRET_KEY"] = secret_key.FLASK_SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pokemon.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+pokemon_database = SQLAlchemy(app)
+
 
 from .views import views
 from .auth import auth
 
+
 app.register_blueprint(views, url_prefix="/")
 app.register_blueprint(auth, url_prefix="/")
+
+
 
 # import Pokemon.about        # Linked via Setup.py
 # import Pokemon.game          # Link pokemon Game
--- Pokemon/db.py
+++ Pokemon/db.py
@@ -1,13 +1,74 @@
+from os import error
+
 import sqlite3
+from sqlite3.dbapi2 import Cursor
+import names
 
-import click 
-from flask import current_app, g
-from flask.cli import with_appcontext
+def link_connection(db_file):
+    conn = False
+
+    try: 
+        conn = sqlite3.connect(db_file)
+    
+        return conn
+    except error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn, sql_create_table_command):
+    table_cursor = conn.cursor()
+    
+    table_cursor.execute(sql_create_table_command)
 
 
-def get_db():
-    pass
+def insert_to_pokemon_table(conn):
+    cur = conn.cursor()
 
-def close_db():
-    pass
+    pokemon_dict = names.poke_names
+    
+    for item in pokemon_dict:
+        pokemon_value =  pokemon_dict[item]
 
+        # print (pokemon_value, pokemon_key)
+        #print (pokemon_value)
+
+        cur.execute("""INSERT INTO pokemon VALUES (?, ?, ?)""", (None, pokemon_value, item))
+    
+    conn.commit()
+
+
+
+def pokemon_image_number():
+    all_pokemon_value = names.poke_names
+
+    for value in all_pokemon_value:
+        # print (all_pokemon_value[value])
+        return all_pokemon_value[value]
+
+def dictionary_pokemon_names():
+    all_pokemon_key = names.poke_names
+
+    for key in all_pokemon_key:
+        # print (key)
+        return key
+
+def main():
+    path = "C:\\users\\kevin\\desktop\\pokemongame\\pokemon\\pokemon.db" 
+
+    create_table_command = """CREATE TABLE IF NOT EXISTS pokemon (
+                                    id integer PRIMARY KEY AUTOINCREMENT,
+                                    image_num TEXT NOT NULL,
+                                    name TEXT NOT NULL
+    );"""
+
+    conn = link_connection(path)
+
+    if conn is not None:
+        create_table(conn, create_table_command)
+        insert_to_pokemon_table(conn)
+    else:
+        print("Something happened.")
+
+if __name__ == "__main__":
+    main()
